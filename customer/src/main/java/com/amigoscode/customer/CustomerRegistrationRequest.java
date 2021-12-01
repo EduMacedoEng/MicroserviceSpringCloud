584cd6f809d4cbbde4d55a0a4a3b08da7163233e
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2021-12-01 03:20:11 -0300

    4 - Postgres and PGAdmin on Docker

--- customer/src/main/java/com/amigoscode/customer/CustomerRegistrationRequest.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerRegistrationRequest.java
@@ -1,5 +1,8 @@
 package com.amigoscode.customer;
 
-public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
 
 }
